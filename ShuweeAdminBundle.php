116b97f042e09a647ed8a4f7b2a512d25622cdb7
Wanjee <wanjee.be@gmail.com>
2014-06-27 17:27:21 +0200

    Add bundle file, fix namespace

---
+++ ShuweeAdminBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author Wanjee <wanjee.be@gmail.com>
+ */
+
+namespace Shuwee\Bundle\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShuweeAdminBundle extends Bundle
+{
+
+}
