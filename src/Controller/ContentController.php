45b6b9306c95c92a975247e3c2dc46d963c42e23
wanjee <wanjee.be@gmail.com>
2015-03-27 16:00:54 +0100

    Implement create, update and delete entity forms: minimal functionality

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -14,11 +14,7 @@
 namespace Wanjee\Shuwee\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Form\Form;
-
 use Wanjee\Shuwee\AdminBundle\Admin\Admin;
 
 /**
@@ -72,8 +68,25 @@ class ContentController extends Controller
     {
         // FIXME : Secure access
 
+        // prepare entity
+        $entityClass = $admin->getEntityClass();
+        $entity = new $entityClass();
+
+        $form = $this->getCreateForm($admin, $entity);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'created');
+            return $this->redirect($this->getRoutingHelper()->generateUrl($admin, 'index'));
+        }
+
         return $this->render('ShuweeAdminBundle:Content:create.html.twig', array(
             'admin' => $admin,
+            'form' => $form->createView(),
           )
         );
     }
@@ -86,16 +99,28 @@ class ContentController extends Controller
     {
         // FIXME : Secure access
 
-        // load entity
         $entity = $admin->loadEntity($request->attributes->get('id'));
 
         if (!$entity) {
             throw $this->createNotFoundException('That resource cannot be found');
         }
 
+        $form = $this->getUpdateForm($admin, $entity);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'updated');
+            return $this->redirect($this->getRoutingHelper()->generateUrl($admin, 'index'));
+        }
+
         return $this->render('ShuweeAdminBundle:Content:update.html.twig', array(
             'admin' => $admin,
             'entity' => $entity,
+            'form' => $form->createView(),
           )
         );
     }
@@ -107,17 +132,83 @@ class ContentController extends Controller
     public function deleteAction(Request $request, Admin $admin)
     {
         // FIXME : Secure access
+
         $entity = $admin->loadEntity($request->attributes->get('id'));
 
         if (!$entity) {
             throw $this->createNotFoundException('That resource cannot be found');
         }
 
+        $form = $this->getDeleteForm($admin, $entity);
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($entity);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'deleted');
+            return $this->redirect($this->getRoutingHelper()->generateUrl($admin, 'index'));
+        }
+
         return $this->render('ShuweeAdminBundle:Content:delete.html.twig', array(
             'admin' => $admin,
             'entity' => $entity,
+            'form' => $form->createView(),
           )
         );
+    }
+
+    /**
+     * Get form to create a new entity.
+     * @param \Wanjee\Shuwee\AdminBundle\Admin\Admin $admin
+     * @param $entity
+     */
+    private function getCreateForm(Admin $admin, $entity)
+    {
+        $formClass = $admin->getForm();
+        $formType = new $formClass();
+
+        $form = $this->createForm($formType, $entity, array(
+          'action' => $this->getRoutingHelper()->generateUrl($admin, 'create'),
+          'method' => 'POST',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Get form to update an existing entity.
+     * @param \Wanjee\Shuwee\AdminBundle\Admin\Admin $admin
+     * @param $entity
+     */
+    private function getUpdateForm(Admin $admin, $entity)
+    {
+        $formClass = $admin->getForm();
+        $formType = new $formClass();
+
+        $form = $this->createForm($formType, $entity, array(
+          'action' => $this->getRoutingHelper()->generateUrl($admin, 'update', array('id' => $entity->getId())),
+          'method' => 'PUT',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+
+    /**
+     * Get form to update an existing entity.
+     * @param \Wanjee\Shuwee\AdminBundle\Admin\Admin $admin
+     * @param $entity
+     */
+    private function getDeleteForm(Admin $admin, $entity)
+    {
+        return $this->createFormBuilder()
+          ->setAction($this->getRoutingHelper()->generateUrl($admin, 'delete', array('id' => $entity->getId())))
+          ->setMethod('DELETE')
+          ->add('submit', 'submit', array('label' => 'Delete', 'attr' => array('class' => 'btn-danger')))
+          ->getForm();
     }
 
     /**
