2e9b14ef996c1a3c8f72048b7d34ab8612647211
wanjee <wanjee.be@gmail.com>
2015-05-21 19:07:52 +0200

    Add DatagridTypeDate class

---
+++ src/Datagrid/Type/DatagridTypeDate.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Wanjee\Shuwee\AdminBundle\Datagrid\Type;
+
+
+/**
+ * Class DatagridTypeDate
+ * @package Wanjee\Shuwee\AdminBundle\Datagrid\Type
+ */
+class DatagridTypeDate extends DatagridType
+{
+    /**
+     * Get administrative name of this type
+     * @return string Name of the type
+     */
+    public function getName()
+    {
+        return 'datagrid_date';
+    }
+
+    /**
+     * Get template block name for this type
+     * @return string Block name (must be a valid block name as defined in Twig documentation)
+     */
+    public function getBlockName()
+    {
+        return 'datagrid_date';
+    }
+
+    /**
+     * Get prepared view parameters
+     *
+     * @param \Wanjee\Shuwee\AdminBundle\Datagrid\Field\DatagridFieldInterface $field
+     * @param mixed $entity Instance of a model entity
+     *
+     * @return mixed
+     */
+    public function getBlockVariables($field, $entity)
+    {
+        // Date_format should be any format supported by http://php.net/manual/en/function.date.php
+        $date_format = $field->getOption('date_format', 'c');
+
+        $date = $field->getData($entity);
+
+        if (!$date instanceof \DateTime) {
+            throw new \Exception(sprintf('Datagrid \'date\' type expects an instance of Datetime. \'%s\' given for field \'%s\'.', gettype($date), $field->getName()));
+        }
+
+        return array(
+          'value' => $date->format($date_format),
+          'datetime' => $date->format(\DateTime::RFC3339),
+        );
+    }
+}
