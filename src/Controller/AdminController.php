e3a594e80da14f6eb04f440488127123b0338507
Wanjee <wanjee.be@gmail.com>
2015-05-26 21:22:27 +0200

    Remove useless import

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,7 +4,6 @@ namespace Wanjee\Shuwee\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
  * Class AdminController
