485e35d0d97444eb2dd412b3e1a0e03e9fad0f42
Wanjee <wanjee.be@gmail.com>
2015-06-05 19:54:03 +0200

    Add simple form extension to add a help text to any form field

---
+++ src/Form/Extension/HelpExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Wanjee\Shuwee\AdminBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+/**
+ * Class HelpExtension
+ * @package Wanjee\Shuwee\AdminBundle\Form\Extension
+ */
+class HelpExtension extends AbstractTypeExtension
+{
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType()
+    {
+        return 'form';
+    }
+
+    /**
+     * Add preview_base_path option
+     *
+     * @param \Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setOptional(array('help'));
+    }
+
+    /**
+     * Build preview url and meta information (i.e. file type)
+     *
+     * @param \Symfony\Component\Form\FormView $view
+     * @param \Symfony\Component\Form\FormInterface $form
+     * @param array $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        if (array_key_exists('help', $options)) {
+            $view->vars['help'] = $options['help'];
+        }
+    }
+}
