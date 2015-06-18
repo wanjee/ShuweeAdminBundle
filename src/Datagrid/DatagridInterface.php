04f8b9705699d7844b5f0c8be5f0004059f9ce37
Wanjee <wanjee.be@gmail.com>
2015-06-18 22:26:40 +0200

    Remove useless defined method from interface, it will not be used

--- src/Datagrid/DatagridInterface.php
+++ src/Datagrid/DatagridInterface.php
@@ -6,14 +6,5 @@ use Symfony\Component\HttpFoundation\Request;
 
 interface DatagridInterface
 {
-    // add field
-    // get fields
-    // add filter
-    // get filters
-
-    /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     */
-    public function setRequest(Request $request);
-
+    // Todo define scope...
 }
