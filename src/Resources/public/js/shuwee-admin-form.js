d7925153b1ebf696d7c2b01e198dbb7f608aefa4
Wanjee <wanjee.be@gmail.com>
2015-07-05 21:19:41 +0200

    All assets are moved to /assets and will be generated by Grunt so clean up

--- src/Resources/public/js/shuwee-admin-form.js
+++
@@ -1,6 +0,0 @@
-/* file_widget */
-
-$('.file-widget-container .file-preview a').click(function(e) {
-    $(this).closest('.file-widget-container').removeClass('show-preview');
-    e.preventDefault();
-});
