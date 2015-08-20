89601e4bd9a75b8fce69ab2dc15e19dd6b423e43
Jeroen Meeus <jme@emakina.com>
2015-08-20 15:00:05 +0200

    We now suport add/remove options for Collection form type with a default prototype_name

--- src/Resources/public/js/shuwee_admin/forms.js
+++ src/Resources/public/js/shuwee_admin/forms.js
@@ -8,4 +8,33 @@
         e.preventDefault();
     });
 
+    function addItemForm($addButton) {
+        var prototype = $addButton.data('prototype'),
+            index = $addButton.data('index'),
+            newForm = prototype.replace(/__name__/g, index),
+            $newRow = $addButton.parent().clone().empty();
+
+        // increase the index with one for the next item
+        $addButton.data('index', index + 1);
+
+        $newRow.html(newForm);
+        $addButton.parent().before($newRow);
+    }
+
+    jQuery(document).ready(function() {
+        $('button[data-prototype]').each(function() {
+            var $addButton = $(this);
+
+            $addButton.on('click', function(e) {
+                e.preventDefault();
+                addItemForm($addButton);
+            });
+        });
+
+        $('button[data-delete]').on('click', function(e) {
+            e.preventDefault();
+            $(this).parent().remove();
+        });
+    });
+
 })();
