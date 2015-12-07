7014c0d5692e07a3ee6591c146c134ad44584cf4
Wanjee <wanjee.be@gmail.com>
2015-12-07 16:59:04 +0100

    Define Datagrid methods

--- src/Datagrid/DatagridInterface.php
+++ src/Datagrid/DatagridInterface.php
@@ -2,7 +2,50 @@
 
 namespace Wanjee\Shuwee\AdminBundle\Datagrid;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 interface DatagridInterface
 {
-    // Todo define scope...
+    /**
+     * Configure options
+     *
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver);
+
+    /**
+     * Get implementation of Admin to use in this datagrid
+     *
+     * @return \Wanjee\Shuwee\AdminBundle\Admin\Admin
+     */
+    public function getAdmin();
+
+    /**
+     * Add a field to the datagrid.
+     *
+     * @param string $name
+     * @param string $type A valid DatagridType implementation name
+     * @param array $options List of options for the given DatagridType
+     */
+    public function addField($name, $type, $options = array());
+
+    /**
+     * Return list of all fields configured for this datagrid
+     */
+    public function getFields();
+
+    /**
+     * Returns the pagination object
+     *
+     * @return
+     */
+    public function getPagination();
+
+    /**
+     * Bind the Request to the Datagrid
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     */
+    public function bind(Request $request);
 }
