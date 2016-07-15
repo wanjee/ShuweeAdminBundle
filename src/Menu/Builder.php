9fbae4d9e1ae4f713231f106a2df92ff1db26102
Sébastien Piraux <spi@emakina.com>
2016-07-15 15:01:12 +0200

    Use the default current logout path instead of Shuwee one when using a different firewall.  (Very) light redesign of sidebar

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -20,38 +20,7 @@ class Builder implements ContainerAwareInterface
     {
         $this->container = $container;
     }
-
-    /**
-     * User menu builder method
-     *
-     * @param FactoryInterface $factory
-     * @param array $options
-     * @return \Knp\Menu\ItemInterface
-     */
-    public function userMenu(FactoryInterface $factory, array $options)
-    {
-        $menu = $factory->createItem('root');
-        $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right navbar-user');
-
-        /** @var \Symfony\Component\Security\Core\Authentication\Token\TokenInterface $securityToken */
-        /** @var \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $securityAuthorizationChecker */
-        $securityToken = $this->container->get('security.token_storage');
-        $securityAuthorizationChecker = $this->container->get('security.authorization_checker');
-
-        if ($securityAuthorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $username = $securityToken->getToken()->getUser()->getUsername();
-
-            $userMenuItem = $menu->addChild($username, array('route' => 'shuwee_logout'))
-                ->setAttribute('dropdown', true)
-                ->setAttribute('icon', 'glyphicon-user');
-            $userMenuItem->addChild('security.logout.action', array('route' => 'shuwee_logout'))
-                ->setAttribute('icon', 'glyphicon-off')
-                ->setExtra('translation_domain', 'ShuweeAdminBundle');
-        }
-
-        return $menu;
-    }
-
+    
     /**
      * Build main menu.
      *
