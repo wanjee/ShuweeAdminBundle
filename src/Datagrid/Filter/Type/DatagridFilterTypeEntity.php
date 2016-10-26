9a2e2bec5c980c7d70d24c82efa8eadca44523b0
JeffBeckers <jeffrey.beckers1903@gmail.com>
2016-10-26 16:45:15 +0200

    #54
    Create DatagridFilterTypeEntity

---
+++ src/Datagrid/Filter/Type/DatagridFilterTypeEntity.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Wanjee\Shuwee\AdminBundle\Datagrid\Filter\Type;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class DatagridFilterTypeEntity
+ * @package Wanjee\Shuwee\AdminBundle\Datagrid\Filter\Type
+ */
+class DatagridFilterTypeEntity extends DatagridFilterType
+{
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver
+            ->setRequired(
+                [
+                    'class',
+                ]
+            )
+            ->setDefined(
+                [
+                    'choice_label'
+                ]
+            )
+            ->setDefault('placeholder', 'All')
+            ->setAllowedTypes('placeholder', ['string']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFormType()
+    {
+        return EntityType::class;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function formatValue($rawValue)
+    {
+        return $rawValue;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCriteriaExpression()
+    {
+        return 'eq';
+    }
+}
