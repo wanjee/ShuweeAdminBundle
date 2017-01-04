07fcd83886d437f3588d550241dd11f5266418e5
Sébastien Piraux <spi@emakina.com>
2017-01-04 16:55:47 +0100

    Only allow ROLE_SUPER_ADMIN to set user roles to prevent privilege escalation

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,9 @@ namespace Wanjee\Shuwee\AdminBundle\Form;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Wanjee\Shuwee\AdminBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -15,11 +18,30 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class UserType extends AbstractType
 {
     /**
+     * @var AuthorizationChecker
+     */
+    private $authorizationChecker;
+
+    /**
+     * ProgressionExtension constructor.
+     * @param AuthorizationChecker $authorizationChecker
+     */
+    public function __construct(AuthorizationChecker $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'onPreSetData')
+        );
+
         $builder
             ->add(
                 'username',
@@ -43,19 +65,35 @@ class UserType extends AbstractType
                     ],
                     'help' => 'Leave empty to keep previous password.'
                 ]
-            )
-            ->add(
-                'roles',
-                ChoiceType::class,
-                [
-                    'choices' => [
-                        'Admin' => 'ROLE_ADMIN',
-                        'Super admin' => 'ROLE_SUPER_ADMIN',
-                    ],
-                    'multiple' => true,
-                    'expanded' => true,
-                ]
             );
+    }
+
+    /**
+     * Set appropriate field elements in form
+     * @param FormEvent $event
+     */
+    function onPreSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        if ($this->authorizationChecker->isGranted('ROLE_SUPER_ADMIN')) {
+            $roles = [
+                'User' => 'ROLE_USER',
+                'Admin' => 'ROLE_ADMIN',
+                'Super admin' => 'ROLE_SUPER_ADMIN',
+            ];
+            $form
+                ->add(
+                    'roles',
+                    ChoiceType::class,
+                    [
+                        'label' => 'Roles',
+                        'required' => true,
+                        'expanded' => true,
+                        'multiple' => true,
+                        'choices' => $roles,
+                    ]
+                );
+        }
     }
 
     /**
